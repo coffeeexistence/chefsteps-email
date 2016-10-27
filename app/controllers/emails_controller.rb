8a7cdf5836d711941dd7073b672e4d5f8028b802
JOHNANTHONY <johnanthony.dev@gmail.com>
2016-10-27 10:28:06 -0700

    Emails are now ordered in emails index

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,7 +1,7 @@
 class EmailsController < ApplicationController
   def index
     before = Time.now
-    @emails = User.distinct.pluck(:email)
+    @emails = User.distinct.order(:id).pluck(:email)
     response_time_seconds = (Time.now - before)
     @comparisions = Comparisons.new(amount: @emails.count, time_taken: response_time_seconds)
     @response_time_ms = (response_time_seconds * 1000).to_i
